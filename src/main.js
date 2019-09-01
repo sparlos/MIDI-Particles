bc903984fe9aad02351818e13f58c0ecbb7d72a3
Stephen Sparling <sparlingcreations@gmail.com>
2019-09-01 11:32:15 -0400

    add vue-js-modal

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueYoutube from 'vue-youtube'
+import VModal from 'vue-js-modal'
 
 Vue.config.productionTip = false
-Vue.use(VueYoutube);
+Vue.use(VueYoutube)
+Vue.use(VModal)
 
 new Vue({
   render: h => h(App),
