81160cf878643cc076de6f385254319f91765116
Stephen Sparling <sparlingcreations@gmail.com>
2019-09-19 10:22:16 -0400

    advanced settings/reset all settings option

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,12 +1,8 @@
 <template>
   <div class="input">
     <div class="input__text">
-      <div class="input__title">
-        {{title}}
-      </div>
-      <div class="input__subtitle">
-        {{subtitle}}
-      </div>
+      <div class="input__title">{{title}}</div>
+      <div class="input__subtitle">{{subtitle}}</div>
     </div>
     <div class="input__form">
       <slot></slot>
@@ -16,13 +12,12 @@
 
 <script>
 export default {
-  name: 'BaseInput',
+  name: "BaseInput",
   props: {
     title: String,
     subtitle: String
   }
-}
-
+};
 </script>
 
 <style scoped lang='scss'>
@@ -48,11 +43,32 @@ export default {
   &__subtitle {
     font-size: 16px;
     margin-top: 5px;
-    color: rgba(0,0,0,0.5)
+    color: rgba(0, 0, 0, 0.5);
   }
 
   &__form {
     margin-left: 80px;
+  }
+}
+
+button {
+  padding: 10px 25px;
+  color: white;
+  background-color: #2699fb;
+  border: none;
+  font-size: 15px;
+  border-radius: 4px;
+  box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.3);
+  transition: 0.2s transform, 0.2s box-shadow;
+
+  &:hover {
+    cursor: pointer;
+    transform: translate(0px, -2px);
+    box-shadow: 3px 3px 12px rgba(0, 0, 0, 0.3);
+  }
+
+  &:active {
+    transform: translate(0px, 1px);
   }
 }
 </style>
